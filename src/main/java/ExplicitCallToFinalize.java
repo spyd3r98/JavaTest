6820e99f9af0d595c5284d8a989559e28a6cdffb
spyd3r98 <54056259+spyd3r98@users.noreply.github.com>
2021-12-10 10:10:55 +0530

    Add files via upload

---
+++ src/main/java/ExplicitCallToFinalize.java
@@ -0,0 +1,17 @@
+package javacodechecker;
+
+public class ExplicitCallToFinalize {
+    public void methodCall(){
+
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE
+        object1.finalize();
+
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE/no-detect
+        object2 = null;
+        // EMB-ISSUE: CodeIssueNames.EXPLICIT_CALL_TO_FINALIZE/no-detect
+        object1.methodCall();
+
+
+
+    }
+}
